d8c0f74586c5d09aef311c142495e9e04d212b65
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-09-11 12:29:40 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -25,7 +25,7 @@ class Localisation:
     
     COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [AnimeSpectrum.mkv]"
 
-    COMPRESS_PROGRESS = "🕛 ETA: {} ♻️ Progress: {}%"
+    COMPRESS_PROGRESS = "🌄 ETA: {} 🌬️🌪️ Progress: {}%"
 
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     
